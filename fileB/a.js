fa6b7c5518ca5129201c12f38d06aad589702744
haozhiqiang <1287079494@qq.com>
2019-10-24 14:23:47 +0800

    测试git diff

--- fileB/a.js
+++ fileB/a.js
@@ -1,10 +1,10 @@
 const a = 1234122;
 function check(){
     let a = 10;
-    let b = [];
+    let b = [2];
     for(let i = 0;i<10;i++){
         b.push(i);
-        return b;
+        return b1;
     }
 }
 check();
